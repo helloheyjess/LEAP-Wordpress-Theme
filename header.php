aab3f5c9c7f3473ab34fe5c87d4202fb0971cd48
helloheyjess <jessicamayblack@gmail.com>
2015-07-06 11:00:41 -0400

    Cleaned up styles

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
 
 	<!-- Fonts -->
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700|Roboto+Slab:400,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700|Bree+Serif:400,700' rel='stylesheet' type='text/css'>
 
 	<style type="text/css">
 		.site-header {
